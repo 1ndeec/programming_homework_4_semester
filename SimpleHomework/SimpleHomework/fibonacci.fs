6bb5f39abfa1967539212dbb90d5fabe248dc632
indeec <murathamatanov@gmail.com>
2026-04-29 14:10:05 +0300

    add first homework

--- SimpleHomework/SimpleHomework/fibonacci.fs
+++ SimpleHomework/SimpleHomework/fibonacci.fs
@@ -1,9 +1,15 @@
-﻿let fibonacci n =
+﻿// Copyright (c) Murat Khamatyanov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+module SimpleHomework.Fibonacci
+
+/// <summary>
+/// Calculates the n-th Fibonacci number.
+/// Throws an ArgumentException if n is negative.
+/// </summary>
+let fibonacci n =
     let rec loop a b i =
-        if i = n then a
-        else loop b (a + b) (i + 1)
+        if i = n then a else loop b (a + b) (i + 1)
 
     if n < 0 then invalidArg "n" "n must be non-negative"
-    elif n = 0 then 0
-    else loop 0 1 1
-
+    else loop 0 1 0
--- SimpleHomework/SimpleHomework/listreverse.fs
+++ SimpleHomework/SimpleHomework/listreverse.fs
@@ -1,8 +1,15 @@
-module Listreverse
+// Copyright (c) Murat Khamatyanov. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+module SimpleHomework.Listreverse
+
+/// <summary>
+/// Reverses the order of elements in a list.
+/// </summary>
 let reverse xs =
     let rec loop acc xs =
         match xs with
         | [] -> acc
         | h :: t -> loop (h :: acc) t
+
     loop [] xs
